177c5f115775fccd1b5c0db290ebc87f51478203
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-28 21:35:51 +0400

    fix

--- feature2/app/main.py
+++ feature2/app/main.py
@@ -1,4 +1,4 @@
-from feature2.app.models.pandas_service import get_quality_control
+from feature2.app.service.pandas_service import get_quality_control
 
 if __name__ == '__main__':
     get_quality_control()
