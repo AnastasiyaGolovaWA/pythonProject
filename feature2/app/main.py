e84edab190c0bb4847ea8fc3ae6dbb97ac5c93ff
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-28 19:38:13 +0400

    fix

--- feature2/app/main.py
+++ feature2/app/main.py
@@ -1,7 +1,7 @@
 import pandas
 import sqlalchemy as db
 
-from feature2.app.models.models import FlowSteps, engine
+from feature2.app.models.models import FlowSteps, engine, SubjectFlowHistory
 
 
 def converter():
@@ -20,6 +20,19 @@ def converter():
     print("QUALITY CONTROL")
     print()
     print(quality_control)
+
+    subject_flow_history = pandas.read_sql_query(
+        sql=db.select([SubjectFlowHistory.anonymous_id,
+                       SubjectFlowHistory.user_id,
+                       SubjectFlowHistory.event_name,
+                       SubjectFlowHistory.timestamp
+                       ]),
+        con=engine
+    )
+
+    print("SUBJECT FLOW HISTORY")
+    print()
+    print(subject_flow_history)
 
 
 if __name__ == '__main__':
--- feature2/app/models/models.py
+++ feature2/app/models/models.py
@@ -1,6 +1,7 @@
 import sqlalchemy as db
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 engine = db.create_engine("postgresql://postgres:root@localhost:5432/BackEnd")
@@ -15,3 +16,12 @@ class FlowSteps(Base):
     color = db.Column(String(8))
     event_name = db.Column(String(64), index=True, unique=True)
     code = db.Column(String(4), nullable=True)
+
+
+class SubjectFlowHistory(Base):
+    __tablename__ = 'subject_flow_history'
+    anonymous_id = db.Column(String(128), primary_key=True)
+    user_id = db.Column(Integer, ForeignKey('user.id'), nullable=True)
+    event_name = db.Column(String(64), ForeignKey('flow_steps.event_name'), primary_key=True)
+    timestamp = db.Column(DateTime(timezone=True), nullable=False)
+
